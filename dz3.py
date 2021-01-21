d8494a463aa1cd0ba5465afe467c5cf9648ecb06
atalantbekuulu <76169843+atalantbekuulu@users.noreply.github.com>
2021-01-21 19:13:40 +0600

    Add files via upload

---
+++ dz3.py
@@ -0,0 +1,24 @@
+
+field=[[], [],[]]
+i=0;
+while i<9:
+    stroka = int(input('введите индекс строки'))
+    stolb = int(input('введите индекс столбца'))
+    if i % 2 == 0:
+        if len(field[stroka]) < 3:
+            field[stroka].insert(stolb, 'X')
+        else:
+            print('эта поле уже заполнено')
+            i=i-1
+
+    else:
+        if len(field[stroka])<3:
+            field[stroka].insert(stolb, 'O')
+        else:
+            print('эта поле уже заполнено')
+            i = i - 1
+    i+=1
+    print(field[0])
+    print(field[1])
+    print(field[2])
+
